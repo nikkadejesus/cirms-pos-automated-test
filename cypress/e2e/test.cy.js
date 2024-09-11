1012073f1c24b73cde996f660e5a9dd5abf08962
Nikka de Jesus <dejesusnikka@gmail.com>
2024-09-11 15:29:38 +0800

    [cirms] add use case in e2e

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -12,4 +12,20 @@ describe('Visit POS staging site', () => {
     cy.get('input[name="password"]').type(`${Cypress.env('password')}`);
     cy.get('input[name="login"]').click();
   });
+
+  it('can select a branch', () => {
+    cy.get('input[name="username"]').type(`${Cypress.env('username')}`);
+    cy.get('input[name="password"]').type(`${Cypress.env('password')}`);
+    cy.get('input[name="login"]').click();
+
+    cy.get('.modal-container').should('be.visible');
+    cy.get('.modal-body').should('be.visible');
+    cy.get('input[value="Login"]').should('be.visible');
+
+    cy.get('[class="chosen phone-number-dropdown"]').click().then(() => {
+      cy.get('[class="chosen phone-number-dropdown"]').within(() => {
+        cy.get('input[type="text"]').click().type(`6 - testing{enter}`);
+      });
+    });
+  });
 });
